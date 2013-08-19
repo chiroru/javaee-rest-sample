d01d7e6cbcd4f66c16c5863cfa5f255790e9d5fa
shinichi-hatano <shinichi.hatano@ctc-g.co.jp>
2013-08-19 17:57:27 +0900

    Integration Test 実装.

--- src/integration-test/java/jp/co/ctc_g/javaee_rest_sample/presentation/SampleIntegrationTest.java
+++ src/integration-test/java/jp/co/ctc_g/javaee_rest_sample/presentation/SampleIntegrationTest.java
@@ -7,17 +7,18 @@ import static com.jayway.restassured.RestAssured.*;
 import com.jayway.restassured.response.Response;
 import java.util.List;
 import java.util.Map;
+import jp.co.ctc_g.javaee_rest_sample.service.domain.Movie;
 import jp.co.ctc_g.javaee_rest_sample.util.integration.IntegrationTest;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
 
 @Category(IntegrationTest.class)
-public class SampleTest {
+public class SampleIntegrationTest {
 
     private final static String BASE_URI = "http://127.0.0.1:8080";
     private final static int PORT = 80;
-    private final static String CONTEXT_PATH = "/javaee-rest-sample";
+    private final static String CONTEXT_PATH = "/javaee-rest-sample-1.0-SNAPSHOT";
 
     static {
         baseURI = BASE_URI;
@@ -27,13 +28,9 @@ public class SampleTest {
 
     @Test
     public void sampleTest() throws Exception {
-        Response resp = get("/webresources/movies");
+        Response resp = given().contentType("application/json; charset=UTF-8").get("/webresources/movies");
         System.out.println(resp.getBody().asString());
         // {"id":1,"name":"The Matrix","actors":"Keanu Reeves, Laurence Fishburne, Carrie-Ann Moss"}
-        List<?> list = get("/webresources/movies").path("");
-        for (Object o : list) {
-            System.out.println(o.toString());
-        }
         Map<Object, Object> m = get("/webresources/movies").path("[0]");
         for (Object key : m.keySet()) {
             System.out.println("[" + key + "] : (" + m.get(key) + ")");
@@ -41,5 +38,29 @@ public class SampleTest {
         System.out.println(get("/webresources/movies").path("[0].id"));
         int id = get("/webresources/movies").path("[0].id");
         assertThat(id, is(1));
+    }
+
+    @Test
+    public void データを登録()
+            throws Exception {
+        Movie m = new Movie(new Long(10), "Regist Test", "Regist Test");
+        Response r = given().contentType("application/json; charset=UTF-8").body(m).post("/webresources/movies");
+        System.out.println(r.getBody().toString());
+    }
+
+    @Test
+    public void データを更新()
+            throws Exception {
+        Movie m = get("/webresources/movies/1").as(Movie.class);
+        m.setName(m.getName() + " modified");
+        Response r = given().contentType("application/json; charset=UTF-8").body(m).put("/webresources/movies");
+        System.out.println(r.getBody().toString());
+    }
+
+    @Test
+    public void データを削除()
+            throws Exception {
+        Response r = delete("/webresources/movies/3");
+        System.out.println(r.getBody().toString());
     }
 }
--- src/main/java/jp/co/ctc_g/javaee_rest_sample/presentation/MovieResource.java
+++ src/main/java/jp/co/ctc_g/javaee_rest_sample/presentation/MovieResource.java
@@ -3,7 +3,11 @@ package jp.co.ctc_g.javaee_rest_sample.presentation;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -20,13 +24,42 @@ public class MovieResource {
     @GET
     @Path("{id}")
     @Produces({"application/xml", "application/json"})
-    public Movie find(@PathParam("id") Integer id) {
+    @Consumes({"application/xml", "application/json"})
+    public Movie find(@PathParam("id") Long id) {
         return service.findById(id);
     }
 
     @GET
     @Produces({"application/xml", "application/json"})
+    @Consumes({"application/xml", "application/json"})
     public List<Movie> findAll() {
         return service.findAll();
+    }
+
+    @POST
+    @Produces({"application/xml", "application/json"})
+    @Consumes({"application/xml", "application/json"})
+    public Movie regist(Movie m) {
+        service.regist(m);
+        return m;
+    }
+
+    @PUT
+    @Produces({"application/xml", "application/json"})
+    @Consumes({"application/xml", "application/json"})
+    public Movie update(Movie m) {
+        service.update(m);
+        return m;
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    @Consumes({"application/xml", "application/json"})
+    public Movie deleteById(@PathParam("id") Long id) {
+        System.out.println("==================>[" + id + "]");
+        Movie m = service.findById(id);
+        service.removeById(id);
+        return m;
     }
 }
